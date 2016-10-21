90a8bb7c7e455089f041b67431b6b54037c1f5e8
Leonel Oliveira <leoneloliver@gmail.com>
2016-10-21 11:42:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+$('#up').on('click', function(){
+  var a = $(".ball").offset({ top: $(".ball").offset().top-10 });
+  $(".ball").css({"top":a});
+  win();
+});
+
+$('#down').on('click', function(){
+  var a = $(".ball").offset({ top: $(".ball").offset().top+10 });
+  $(".ball").css({"top":a});
+  win();
+});
+
+$('#left').on('click', function(){
+  var a = $(".ball").offset({ left: $(".ball").offset().left-10 });
+  $(".ball").css({"top":a});
+  win();
+});
+
+$('#right').on('click', function(){
+  var a = $(".ball").offset({ left: $(".ball").offset().left+10 });
+  $(".ball").css({"top":a});
+  win();
+});
+
+$('.ball').on('click', function(){
+  alert("Top position is: "+$(this).offset().top+" and Left position is: "+$(this).offset().left);
+  $(this).css({"top":"46%","left":"46%"});
+  win();
+});
+
+function win(){
+  if($(".ball").offset().top==327 && $(".ball").offset().left == 327 ){
+    alert("you win!");
+    $(".ball").css({"top":"46%","left":"46%"});
+  }
+  if($(".ball").offset().top==27 && $(".ball").offset().left == 327 ){
+    alert("you win!");
+    $(".ball").css({"top":"46%","left":"46%"});
+  }
+}
