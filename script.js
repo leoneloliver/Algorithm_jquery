dbbdb606cc1cd49f03cb1d7bfb99b86357b284c3
Leonel Oliveira <leoneloliver@gmail.com>
2016-11-07 13:35:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,26 @@ $('#up').on('click', function(){
   $(".ball").css({"top":a});
   win();
 });
-
 $('#down').on('click', function(){
   var a = $(".ball").offset({ top: $(".ball").offset().top+10 });
   $(".ball").css({"top":a});
   win();
 });
-
 $('#left').on('click', function(){
   var a = $(".ball").offset({ left: $(".ball").offset().left-10 });
   $(".ball").css({"top":a});
   win();
 });
-
 $('#right').on('click', function(){
   var a = $(".ball").offset({ left: $(".ball").offset().left+10 });
   $(".ball").css({"top":a});
   win();
 });
-
 $('.ball').on('click', function(){
   alert("Top position is: "+$(this).offset().top+" and Left position is: "+$(this).offset().left);
   reposition();
   win();
 });
-
 function win(){
   if($(".ball").offset().top==327 && $(".ball").offset().left == 327 ){
     alert("you win!");
@@ -38,7 +33,6 @@ function win(){
     reposition();
   }
 }
-
 function reposition(){
   $(".ball").css({"top":"46%","left":"46%"});
 }
